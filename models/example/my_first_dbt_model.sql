0a39f6ab803e833663ba6071f9b8aba2026d37a0
darwin.pactoranan.wrk@outlook.com <darwin.pactoranan.wrk@outlook.com>
2024-04-02 17:28:44 +0000

    first test commit

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,5 +1,7 @@
 
 /*
+    test change for repo commit
+   
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
