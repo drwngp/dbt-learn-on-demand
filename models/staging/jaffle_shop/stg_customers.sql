f4c3e22911d191af1bbdaa2f0370ba53ac7ce711
Darwin Pactoranan <darwin.pactoranan.wrk@outlook.com>
2024-04-25 10:06:29 +0000

    - change stg_payments to incremental load. - add created_at colun in stg_customers.

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -12,6 +12,7 @@ with
             customers.id as customer_id 
             , customers.first_name as customer_first_name
             , customers.last_name as customer_last_name 
+            , {{ current_timestamp_in_utc_backcompat() }} as created_dt  
         from customers 
 
     )
